b73cbf5379f4fb9c4a8abaf1878cadb23558c4f6
chrom-P <kkllmm104@gmail.com>
2021-11-30 10:49:50 +0900

    up

---
+++ Gauss-Seidel-Iterative-Method.c
@@ -0,0 +1,36 @@
+/*Gauss-Seidel Iterative Method*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define N 4
+int main(void){
+    int i,j,n=0;
+    float A[N][N],x[N],b[N],t[N],er;
+    FILE *fp;
+    if( (fp=fopen("20201028-1.dat","r"))==NULL ) {
+        printf("cannot open file.\n");
+        exit(0);
+    }
+    for(i=0;i<N;i++) {
+    for(j=0;j<N;j++) fscanf(fp,"%f",&A[i][j]);
+    fscanf(fp,"%f",&b[i]);
+    x[i]=0.0;
+    }
+    fclose(fp);
+    do {
+        er=0.0;
+        for(i=0;i<N;i++) {
+            t[i]=b[i];
+            for(j=0;j<N;j++)
+            if(i!=j) t[i]-=A[i][j]*x[j];
+        t[i]/=A[i][i];
+        er+=fabs(t[i]-x[i]);
+        x[i]=t[i];
+        }
+        n++;
+    } while(er>1.0e-4);
+    printf("n=%d\n",n);
+    for(i=0;i<N;i++) printf(" %7.3f",x[i]);
+    printf("\n");
+}
